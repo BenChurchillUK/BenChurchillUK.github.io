a2fedfc1c353026120e438b71d7fed7f98241bd0
Ben Churchill <167097785+BenChurchillUK@users.noreply.github.com>
2025-02-05 13:23:46 +0000

    Update worldmap.js

--- Scripts/worldmap.js
+++ Scripts/worldmap.js
@@ -1,3 +1,31 @@
-var map = d3.geomap()
-    .geofile('/d3-geomap/topojson/world/countries.json')
-    .draw(d3.select('#map'));
+// Economic World Map
+
+let EconWorldMapWidth = 1000;
+let EconWorldMapHeight = 800;
+
+let EconWorldMapProjection = d3
+    .geoEquirectangular()
+;
+
+let EconWorlMapPath = ds
+    .geoPath()
+    .projection(EconomicWorldMapProjection)
+;
+
+let EconWorldMapSVG = d3.select("#svganchor")
+    .append("svg")
+    .attr("width", EconWorldMapWidth)
+    .attr("height", EconWorldMapHeight)
+;
+
+d3.json("https://raw.githubusercontent.com/BenChurchillUK/BenChurchillUK.github.io/refs/heads/main/Assets/worldmap.json", (json) => {
+
+svg.selectAll("EconWorldMapPath")
+    .data(json.features)
+    .enter()
+    .append("EconWorldMapPath")
+    .attr("d", path)
+    .attr("stroke", "dimgray")
+    .attr("fill", (d, i) => {return color(i)});
+});
+
